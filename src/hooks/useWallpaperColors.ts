9754b27d972fd8b26de3487ac7b91978ac96ac11
7777Danil <shametkodanil@tandex.ru>
2026-06-14 15:45:34 +0700

    Replace colorthief with custom Canvas color extraction

--- src/hooks/useWallpaperColors.ts
+++ src/hooks/useWallpaperColors.ts
@@ -1,6 +1,4 @@
 ﻿import { useEffect, useState } from 'react';
-// @ts-ignore
-import ColorThief from 'colorthief';
 
 export const useWallpaperColors = (imageSrc: string) => {
   const [primary, setPrimary] = useState('#ffffff');
@@ -12,8 +10,24 @@ export const useWallpaperColors = (imageSrc: string) => {
     img.crossOrigin = 'Anonymous';
     img.src = imageSrc;
     img.onload = () => {
-      const colorThief = new ColorThief();
-      const [r, g, b] = colorThief.getColor(img);
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      if (!ctx) return;
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+      const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
+      
+      let r = 0, g = 0, b = 0;
+      for (let i = 0; i < imageData.length; i += 4) {
+        r += imageData[i];
+        g += imageData[i+1];
+        b += imageData[i+2];
+      }
+      const pixelCount = imageData.length / 4;
+      r = Math.floor(r / pixelCount);
+      g = Math.floor(g / pixelCount);
+      b = Math.floor(b / pixelCount);
       const rgb = `rgb(${r}, ${g}, ${b})`;
       setPrimary(rgb);
 
