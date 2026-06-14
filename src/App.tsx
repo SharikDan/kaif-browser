5026d29fa7d0f877c933af12b5a209eecf7a56bf
7777Danil <shametkodanil@tandex.ru>
2026-06-14 15:26:40 +0700

    Fix App.tsx with correct JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import React from 'react'; 
-import { ThemeProvider } from './contexts/ThemeContext'; 
-import { TabsProvider, useTabs } from './contexts/TabsContext'; 
-import { Background } from './components/Background'; 
-import { TabBar } from './components/TabBar'; 
-import SearchBar from './components/SearchBar'; 
-import { WebView } from './components/WebView'; 
- 
-const AppContent = () =
-  const { tabs, activeTabId, updateTabUrl } = useTabs(); 
-  const activeTab = tabs.find(tab = === activeTabId); 
- 
-  return <> 
-    <Background /> 
-    <TabBar /> 
-    {activeTab?.url ?  
-      <WebView url={activeTab.url} onTitleChange={(title) =
-        if (activeTabId) updateTabUrl(activeTabId, activeTab.url, title); 
-      }} />  
-      :  
-      <SearchBar />} 
-  </>; 
-}; 
- 
-function App() { 
-  return  
-    <ThemeProvider> 
-      <TabsProvider> 
-        <AppContent /> 
-      </TabsProvider> 
-    </ThemeProvider>; 
-} 
- 
-export default App; 
+import React from 'react';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { TabsProvider, useTabs } from './contexts/TabsContext';
+import { Background } from './components/Background';
+import { TabBar } from './components/TabBar';
+import SearchBar from './components/SearchBar';
+import { WebView } from './components/WebView';
+
+const AppContent = () => {
+  const { tabs, activeTabId, updateTabUrl } = useTabs();
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
+
+  return (
+    <>
+      <Background />
+      <TabBar />
+      {activeTab?.url ? (
+        <WebView url={activeTab.url} onTitleChange={(title) => {
+          if (activeTabId) updateTabUrl(activeTabId, activeTab.url, title);
+        }} />
+      ) : (
+        <SearchBar />
+      )}
+    </>
+  );
+};
+
+function App() {
+  return (
+    <ThemeProvider>
+      <TabsProvider>
+        <AppContent />
+      </TabsProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
