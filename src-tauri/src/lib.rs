cf55d3417f93cedd79b783c273ef04c65cc502b1
7777Danil <shametkodanil@tandex.ru>
2026-06-14 17:57:21 +0700

    Fix lib.rs: remove unused get_webview_window block

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,8 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            if let Some(window) = app.get_webview_window("main") {
-                // open devtools to debug
-                // window.open_devtools();
-            }
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
