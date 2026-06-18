35a57fe18b12c96c838ac63349a0dffa85355ec0
7777Danil <shametkodanil@tandex.ru>
2026-06-18 20:28:56 +0700

    Simple working Electron app

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
