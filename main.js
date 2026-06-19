0030f65b35c2f182d1a29cdc86840aeda241e0e2
7777Danil <shametkodanil@tandex.ru>
2026-06-19 14:46:03 +0700

    Add full browser UI with navigation buttons and WebView

--- main.js
+++ main.js
@@ -5,18 +5,31 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
+    minWidth: 800,
+    minHeight: 600,
     frame: false,
     backgroundColor: '#0f0f0f',
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
+      webviewTag: true
     }
   });
 
   win.loadFile('index.html');
+  
+  // Открываем DevTools в режиме разработки
+  // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
