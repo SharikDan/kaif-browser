28b616d08537983c971d84a4e733fb0fb0f7d787
7777Danil <shametkodanil@tandex.ru>
2026-06-19 14:12:48 +0700

    Fresh start: Electron app

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    frame: false,
+    backgroundColor: '#0f0f0f',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
