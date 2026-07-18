e38550111b86445fdc538073f60b49e159225ebf
Danil <shametkod@gmail.com>
2026-07-18 09:22:10 +0700

    perf(preload): return unsubscribe functions for renderer listeners to avoid duplicates

--- preload.js
+++ preload.js
@@ -5,13 +5,36 @@ contextBridge.exposeInMainWorld('electronAPI', {
   maximize: () => ipcRenderer.send('window-maximize'),
   close: () => ipcRenderer.send('window-close'),
   showContextMenu: (link) => ipcRenderer.send('context-menu', link),
-  onOpenNewTab: (callback) => ipcRenderer.on('open-new-tab', (event, link) => callback(link)),
-  onRefresh: (callback) => ipcRenderer.on('refresh-page', () => callback()),
-  onDownloadStarted: (callback) => ipcRenderer.on('download-started', (event, data) => callback(data)),
-  onDownloadProgress: (callback) => ipcRenderer.on('download-progress', (event, data) => callback(data)),
-  onDownloadDone: (callback) => ipcRenderer.on('download-done', (event, data) => callback(data)),
+
+  // Return unsubscribe functions so renderer can avoid duplicate listeners
+  onOpenNewTab: (callback) => {
+    const listener = (event, link) => callback(link);
+    ipcRenderer.on('open-new-tab', listener);
+    return () => ipcRenderer.removeListener('open-new-tab', listener);
+  },
+  onRefresh: (callback) => {
+    const listener = () => callback();
+    ipcRenderer.on('refresh-page', listener);
+    return () => ipcRenderer.removeListener('refresh-page', listener);
+  },
+  onDownloadStarted: (callback) => {
+    const listener = (event, data) => callback(data);
+    ipcRenderer.on('download-started', listener);
+    return () => ipcRenderer.removeListener('download-started', listener);
+  },
+  onDownloadProgress: (callback) => {
+    const listener = (event, data) => callback(data);
+    ipcRenderer.on('download-progress', listener);
+    return () => ipcRenderer.removeListener('download-progress', listener);
+  },
+  onDownloadDone: (callback) => {
+    const listener = (event, data) => callback(data);
+    ipcRenderer.on('download-done', listener);
+    return () => ipcRenderer.removeListener('download-done', listener);
+  },
+
   openDownloadFolder: (folderPath) => ipcRenderer.invoke('open-download-folder', folderPath),
-  getPasswords: () => ipcRenderer.invoke('get-passwords'),
-  savePassword: (domain, username, password) => ipcRenderer.invoke('save-password', { domain, username, password }),
-  getPasswordsForDomain: (domain) => ipcRenderer.invoke('get-passwords-for-domain', domain)
+  getPasswords: () =&gt; ipcRenderer.invoke('get-passwords'),
+  savePassword: (domain, username, password) =&gt; ipcRenderer.invoke('save-password', { domain, username, password }),
+  getPasswordsForDomain: (domain) =&gt; ipcRenderer.invoke('get-passwords-for-domain', domain)
 });
